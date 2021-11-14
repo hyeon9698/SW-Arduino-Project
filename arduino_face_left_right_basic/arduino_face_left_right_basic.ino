d89c8844f03e09677500ea1c5baba54393dced15
Donghyeon Cho <hyeon9698@naver.com>
2021-11-15 02:47:56 +0900

    코드 클린

--- arduino_face_left_right_basic/arduino_face_left_right_basic.ino
+++ arduino_face_left_right_basic/arduino_face_left_right_basic.ino
@@ -7,10 +7,6 @@
 SerialData serialData(2,3); //(numOfValsRec,digitsPerValRec)
 Servo servo;
 Servo servo2;
-/*0 or 1 - 1 digit
-0 to 99 -  2 digits 
-0 to 999 - 3 digits 
- */
 /* 오른쪽 112
  왼쪽 600
 */
@@ -31,38 +27,7 @@ void setup() {
   
 }
 void loop() {
-  //  long duration, distance;
-  // digitalWrite(TRIG, LOW);
-  // delayMicroseconds(2);
-  // digitalWrite(TRIG, HIGH);
-  // delayMicroseconds(10);
-  // digitalWrite(TRIG, LOW);
-  //  duration = pulseIn (ECHO, HIGH); //물체에 반사되어돌아온 초음파의 시간을 변수에 저장합니다.
-  //  distance = duration * 17 / 1000; 
-
-  //   long duration2, distance2;
-  // digitalWrite(TRIG2, LOW);
-  // delayMicroseconds(2);
-  // digitalWrite(TRIG2, HIGH);
-  // delayMicroseconds(10);
-  // digitalWrite(TRIG2, LOW);
-  //  duration2 = pulseIn (ECHO2, HIGH); //물체에 반사되어돌아온 초음파의 시간을 변수에 저장합니다.
-  //  distance2 = duration2 * 17 / 1000; 
-  
   serialData.Get(valsRec);
-  /* if (distance<10)
-   {
-      x=30;
-      y=90;
-      servo.write(y);
-      servo2.write(x);
-    }
-    else if (distance2<10){
-      x=150;
-      y=90;
-      servo.write(y);
-      servo2.write(x);
-    }*/
     while (valsRec[0] != 0&&valsRec[1] != 0){
       serialData.Get(valsRec);
     if (valsRec[0]>370){//x축
@@ -83,13 +48,6 @@ void loop() {
     if (x>180){
       x=180;
     }
-  
-//  if (y<90){
-//    y=90;
-//  }
-//  if (y>180){
-//    y=180;
-//  }
   servo.write(x);
     servo2.write(y);
     delay(100);
