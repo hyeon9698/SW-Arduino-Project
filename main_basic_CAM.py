2fe67ecfc702fae2bfa82f4c03d3ee7d3335a905
Donghyeon Cho <hyeon9698@naver.com>
2021-11-27 14:45:51 +0900

    update

---
+++ main_basic_CAM.py
@@ -0,0 +1,27 @@
+import cv2
+import time
+from cvzone.FaceDetectionModule import FaceDetector
+from cvzone.SerialModule import SerialObject
+
+cap = cv2.VideoCapture(1)
+detector = FaceDetector()
+arduino = SerialObject()
+
+while True:
+    # time.sleep(0.1)
+    success, img = cap.read()
+    if success:
+        img, bboxs = detector.findFaces(img)
+
+        if bboxs:
+            print(bboxs[0]['center'])
+            arduino.sendData([bboxs[0]['center'][0], bboxs[0]['center'][1]])
+        else:
+            arduino.sendData([0, 0])
+        cv2.imshow("Image", img)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    else:
+        break
+cap.release()
+cv2.destroyAllWindows()
